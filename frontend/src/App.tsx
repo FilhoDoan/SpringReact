46de0123cf3054f40e89eb7792c74ff5ae168c25
FilhoDoan <doangalvaofilho@gmail.com>
2022-11-17 18:22:58 -0300

    fist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import Notificationbutton from "./componentes/notificationbutton"
+
 function App() {
-  return(
-    <h1>olamundo</h1>
+  return (
+    <>
+      <h1>olamundo</h1>
+      <Notificationbutton />
+      <Notificationbutton/>
+    </>
   )
 }
 export default App
---
+++ frontend/src/componentes/notificationbutton/index.tsx
@@ -0,0 +1,12 @@
+import icone from '../../assets/img/notificaçãoicone.svg'
+import './styles.css'
+function Notificationbutton() {
+    return (
+        <>
+            <div className="dsmeta-red-btn">
+            <img src={icone} alt="Notificar" />
+        </div>
+            </>
+            )
+}
+export default Notificationbutton
